c2f74f3087d8044c04508845d3cb248cac521286
James <jamesg487@gmail.com>
2022-01-19 11:54:30 +0000

    completed and run forst 3 tests

---
+++ run_tests.py
@@ -0,0 +1,8 @@
+import unittest
+
+from tests.pub_test import TestPub
+
+# python3 run_tests.py, below checks if run_tests.py is the file we want to run and executes the unittest library
+#  __name__ -> this is a variable set by Python, most of the time it will be the name of the file/module
+if __name__ == "__main__":
+    unittest.main()
